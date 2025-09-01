dd0c699cf2382f6b38ca3f26372a137aedef997a
ShivanshTiwari01 <shivanshtiwari2014@gmail.com>
2025-09-02 00:51:12 +0530

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', '@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+};
