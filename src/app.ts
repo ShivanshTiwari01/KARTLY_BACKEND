d403896fc3250f8b1b8eaab5cbced3bcdc92174d
Shivansh Tiwari <shivanshtiwari2014@gmail.com>
2026-04-28 19:54:26 +0530

    chore: basic setup

--- src/app.ts
+++ src/app.ts
@@ -34,18 +34,17 @@ app.use(passport.initialize());
 // If using sessions: app.use(passport.session());
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-  // store: ... , // Redis, Memcached, etc. See below.
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+  ipv6Subnet: 56,
 });
 
 app.use(limiter);
 
 app.get('/', (_req: Request, res: Response) => {
-  res.status(403).json({
+  res.status(401).json({
     error: 'Access denied',
     message: 'Unauthorized access to this resource',
   });
---
+++ src/models/userProfile.model.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import User from './user.model';
+
+const userProfileSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserProfile = mongoose.model('UserProfile', userProfileSchema);
+
+export default UserProfile;
